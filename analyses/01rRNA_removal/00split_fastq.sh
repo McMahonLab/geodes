b037d6e9e94d1cfd77f61d7c0434f5d5a2508b85
alexlinz <amlinz@wisc.edu>
2017-02-13 14:32:14 -0600

    Completed first pass of rRNA removal! scripts and lab notebook stored here.

--- analyses/01rRNA_removal/00split_fastq.sh
+++ analyses/01rRNA_removal/00split_fastq.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-cp /mnt/gluster/amlinz/GEODES_metaT/GEODES001.filter-MTF.fastq.gz ./
-gzip -d GEODES001.filter-MTF.fastq.gz
+cp /mnt/gluster/amlinz/GEODES_metaT/$1.filter-MTF.fastq.gz ./
+gzip -d $1.filter-MTF.fastq.gz
 
 # make a folder for each metagenome
-code=`echo "GEODES001.filter-MTF.fastq" | cut -d'.' -f1`
-mkdir ${code}
+mkdir $1
 
 # Split by number of lines
-split -l 1000000 GEODES001.filter-MTF.fastq ${code}/${code}
+split -l 1000000 $1.filter-MTF.fastq $1/$1
 
 # Rename files to include .fastq extension
-for file in ${code}/*;do mv "$file" "$file.fastq";done
+for file in $1/*;do mv "$file" "$file.fastq";done
 
 # Grace includes a read file in the folder with the split files, but I don't think that makes sense for using with SortmeRNA
 # I'm also not going to zip up the file, because I'd just need to unzip back on gluster
-mv ${code} /mnt/gluster/amlinz/GEODES_metaT_split/
-rm GEODES001.filter-MTF.fastq
+mv $1 /mnt/gluster/amlinz/GEODES_metaT_split/
+rm $1.filter-MTF.fastq
