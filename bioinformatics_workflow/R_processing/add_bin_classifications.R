6d82b5c214527007d329d24d024cc28d3c635735
alexlinz <amlinz@wisc.edu>
2018-03-03 20:07:08 -0600

    Remade expression by phyla plots with new SAGs in database

--- bioinformatics_workflow/R_processing/add_bin_classifications.R
+++ bioinformatics_workflow/R_processing/add_bin_classifications.R
@@ -1,8 +1,8 @@
 # Update gene key files with classifications from binning
 
 # Gene keys
-#mendota_key <- read.csv("D:/geodes_data_tables/Mendota_ID90_genekey.csv", header = T)
-#spark_key <- read.csv("D:/geodes_data_tables/Sparkling_ID90_genekey.csv", header = T)
+mendota_key <- read.csv("D:/geodes_data_tables/Mendota_ID90_genekey.csv", header = T)
+spark_key <- read.csv("D:/geodes_data_tables/Sparkling_ID90_genekey.csv", header = T)
 trout_key <- read.csv("D:/geodes_data_tables/Trout_ID90_genekey.csv", header = T)
 # Binning results
 bins <- read.csv("D:/geodes_data_tables/GEODES_bin_data.csv", header = T)
@@ -15,25 +15,25 @@ keep <- match(contigs$V2, bins$bin)
 contigs <- contigs[which(is.na(keep) == F), ]
 
 # Replace in lake keys
-# mendota_key$Taxonomy <- as.character(mendota_key$Taxonomy)
-# search <- match(mendota_key$Genome, as.character(contigs$V1))
-# where_in_key <- which(is.na(search) == F)
-# where_in_contigs <- search[where_in_key]
-# matching_bins <- contigs$V2[where_in_contigs]
-# taxonomy_add <- as.character(bins$phylodist_taxonomy[match(matching_bins, bins$bin)])
-# mendota_key$Taxonomy[where_in_key] <- taxonomy_add
-# 
-# write.csv(mendota_key, "D:/geodes_data_tables/Mendota_ID90_genekey_reclassified.csv", row.names = F)
+mendota_key$Taxonomy <- as.character(mendota_key$Taxonomy)
+search <- match(mendota_key$Genome, as.character(contigs$V1))
+where_in_key <- which(is.na(search) == F)
+where_in_contigs <- search[where_in_key]
+matching_bins <- contigs$V2[where_in_contigs]
+taxonomy_add <- as.character(bins$phylodist_taxonomy[match(matching_bins, bins$bin)])
+mendota_key$Taxonomy[where_in_key] <- taxonomy_add
 
-# spark_key$Taxonomy <- as.character(spark_key$Taxonomy)
-# search <- match(spark_key$Genome, as.character(contigs$V1))
-# where_in_key <- which(is.na(search) == F)
-# where_in_contigs <- search[where_in_key]
-# matching_bins <- contigs$V2[where_in_contigs]
-# taxonomy_add <- as.character(bins$phylodist_taxonomy[match(matching_bins, bins$bin)])
-# spark_key$Taxonomy[where_in_key] <- taxonomy_add
-# 
-# write.csv(spark_key, "D:/geodes_data_tables/Sparkling_ID90_genekey_reclassified.csv", row.names = F)
+write.csv(mendota_key, "D:/geodes_data_tables/Mendota_ID90_genekey_reclassified_2018-03-03.csv", row.names = F)
+
+spark_key$Taxonomy <- as.character(spark_key$Taxonomy)
+search <- match(spark_key$Genome, as.character(contigs$V1))
+where_in_key <- which(is.na(search) == F)
+where_in_contigs <- search[where_in_key]
+matching_bins <- contigs$V2[where_in_contigs]
+taxonomy_add <- as.character(bins$phylodist_taxonomy[match(matching_bins, bins$bin)])
+spark_key$Taxonomy[where_in_key] <- taxonomy_add
+
+write.csv(spark_key, "D:/geodes_data_tables/Sparkling_ID90_genekey_reclassified_2018-03-03.csv", row.names = F)
 
 trout_key$Taxonomy <- as.character(trout_key$Taxonomy)
 search <- match(trout_key$Genome, as.character(contigs$V1))
@@ -43,4 +43,4 @@ matching_bins <- contigs$V2[where_in_contigs]
 taxonomy_add <- as.character(bins$phylodist_taxonomy[match(matching_bins, bins$bin)])
 trout_key$Taxonomy[where_in_key] <- taxonomy_add
 
-write.csv(trout_key, "D:/geodes_data_tables/Trout_ID90_genekey_reclassified.csv", row.names = F)
+write.csv(trout_key, "D:/geodes_data_tables/Trout_ID90_genekey_reclassified_2018-03-03.csv", row.names = F)
--- bioinformatics_workflow/R_processing/transcripts_per_liter.R
+++ bioinformatics_workflow/R_processing/transcripts_per_liter.R
@@ -1,11 +1,11 @@
 # Normalize table to transcripts/L
 
-readCounts <- read.csv("D:/GEODES2refMAGsSAGs_readcounts.txt", header = T, row.names = 1)
+readCounts <- read.table("D:/geodes_data_tables/Trout_ID90_2018-03-02.readcounts.txt", header = T, row.names = 1, sep = " ")
 sample_data <- read.csv("C:/Users/Alex/Desktop/geodes/bioinformatics_workflow/R_processing/sample_metadata.csv")
 
 # Remove samples with poor std
 std <- readCounts[which(rownames(readCounts) == "pFN18A_DNA_transcript"),]
-readCounts2 <- readCounts[1:248805, which(std > 50)]
+readCounts2 <- readCounts[, which(std > 50)]
 # Calculate standard normalization factor
 
 std2 <- readCounts2[which(rownames(readCounts2) == "pFN18A_DNA_transcript"),]
@@ -24,24 +24,24 @@ for(i in 1:length(norm_val)){
 }
 
 # Save normalized table
-write.csv(readCounts2, "D:/GEODES2refMAGsSAGs_normalized_readcounts.csv", quote = F)
+write.csv(readCounts2, "D:/geodes_data_tables/Trout_ID90_normalized_readcounts.csv", quote = F)
 
 
 # Make a table summed by genome
-all_genomes <- substr(rownames(readCounts2), start = 1, stop = 10)
-genomes <- unique(all_genomes)
-genome_table <- readCounts2[1,]
-
-for(i in 1:length(genomes)){
-  genes <- readCounts2[which(all_genomes == genomes[i]), ]
-  genome_row <- colSums(genes)
-  genome_table <- rbind(genome_table, genome_row)
-}
-
-genome_table <- genome_table[2:267,]
-rownames(genome_table) <- genomes
-
-# save genome table
-
-write.csv(genome_table, "D:/GEODES2refMAGsSAGs_normalized_genomecounts.csv", quote = F)
-
+# all_genomes <- substr(rownames(readCounts2), start = 1, stop = 10)
+# genomes <- unique(all_genomes)
+# genome_table <- readCounts2[1,]
+# 
+# for(i in 1:length(genomes)){
+#   genes <- readCounts2[which(all_genomes == genomes[i]), ]
+#   genome_row <- colSums(genes)
+#   genome_table <- rbind(genome_table, genome_row)
+# }
+# 
+# genome_table <- genome_table[2:267,]
+# rownames(genome_table) <- genomes
+# 
+# # save genome table
+# 
+# write.csv(genome_table, "D:/geodes_data_tables/Trout_ID90_normalized_genomecounts.csv", quote = F)
+# 
